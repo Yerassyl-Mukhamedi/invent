8d37bfe928cd02492b4319773a8c93b9093047c9
Erasyl <erasyl490@gmail.com>
2019-06-26 14:25:17 +0600

    toson1

--- blog/static/js/code.js
+++ blog/static/js/code.js
@@ -1,6 +1,10 @@
 (function() {
-	let url = location.href.match('#(.*)')[1];
-	console.log(url);
-	let sik = document.getElementById(url);
-	sik.style.display = 'none';
+	try {
+		let url = location.href.match('#(.*)')[1];
+		console.log(url);
+		let sik = document.getElementById(url);
+		sik.style.display = 'block';
+	} catch (e) {
+		return true;
+	}
 })();
