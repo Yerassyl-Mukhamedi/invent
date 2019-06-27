096bd93cd0ee9bbeebdde14f20966dc0c1ee9c60
erasyl <erasyl490@gmail.com>
2019-06-27 12:09:14 +0600

    allowedHost

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g)vm(wipsrn1-(()k5)8xc)h07vrr8q@xp#r*oco53)*f^r96m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
